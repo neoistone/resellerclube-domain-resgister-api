ad6ea37242270aa3f98a85e28505e19e97c8aee2
Manikantasripadi <59179052+Manikantasripadi@users.noreply.github.com>
2021-11-20 00:21:36 +0530

    Update resellerclub.php

--- src/resellerclub.php
+++ src/resellerclub.php
@@ -1,54 +1,46 @@
 <?php
-
+namespace neoistone\Resellerclub;
 /**
- * ResellerclubDomain api neoistone version 
- * version 1.0.0
- * website www.neoistone.com
- * API Documentation : freeaccount.myorderbox.com/kb/answer/750
- **/
- namespace neoistone\Resellerclub;
-
+ * @package neoistone
+ * @author Neoistone Technologies
+ * @contact support@eoistone.com
+ */
 class domainapi
 {
-	private $api = null;
+	private $api = "https://httpapi.com/api/domains/";
 	private $api_key = null;
 	private $api_secure = null;
-	private $response = null;
+	private $error = null;
+
+	private $response = "json";
     
     function __construct($user_id,$api_key,$response="json",$environment=true)
 	{
-		if ($environment) {
-		  self::api = "https://httpapi.com/api/domains/";
-		  self::api_key = $api_key;
-		  self::api_secure = $user_id;
-		  self::response = $response;
-		}else{
-		  self::api = "https://test.httpapi.com/api/domains/";
-		  self::api_key = $api_key;
-		  self::api_secure = $user_id;
-		  self::response = $response;
-		}
+		$this->api = (!$environment) ? "https://test.httpapi.com/api/domains/" : "https://httpapi.com/api/domains/";
+	        $this->api_key = $api_key;
+	        $this->api_secure = $user_id;
+	        $this->response = $response;
 	}
     
-    public static function command()
+    public function command()
     {
-       if(!func_num_args() == 2){ self::error ='params not empty'; } else {
+       if(!func_num_args() == 2){ $this->error ='params not empty'; } else {
 		$x = func_get_args();
 		//decting params corret way
 		if(is_array($x['0'])){ $params = $x['0'];  $cmd = $x['1']; }else{ $params = $x['1']; $cmd = $x['0']; }
 		if(is_array($x['1'])){ $params = $x['1'];  $cmd = $x['0']; }else{ $params = $x['0']; $cmd = $x['1']; }
-           self::api .= strtolower($cmd).".".strtolower(self::response);
-           self::api .= "?auth-userid=".self::api_key;
-	   self::api .= "&api-key=".self::api_secure;
+           $this->api .= strtolower($cmd).".".strtolower($this->response);
+           $this->api .= "?auth-userid=".$this->api_key;
+	       $this->api .= "&api-key=".$this->api_secure;
 	   foreach($params as $key => $value) {
-           self::api .= '&'.urlencode($key).'='.urlencode($value);
+           $this->api .= '&'.urlencode($key).'='.urlencode($value);
        }}
     }
 
     public static function run(){
-      if(!empty(self::error)){ return self::error; } else{
+      if(!empty($this->error)){ return $this->error; } else{
     	 $ch = curl_init();
-         curl_setopt($ch,CURLOPT_URL, self::api);
+         curl_setopt($ch,CURLOPT_URL, $this->api);
          curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
          curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
          curl_setopt($ch, CURLOPT_TIMEOUT, 5);
